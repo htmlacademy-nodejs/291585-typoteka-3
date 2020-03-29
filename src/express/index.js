eba5ce5a4c436f154106aa9d714f7db9d87067f0
Valentin <bagadin@inbox.ru>
2020-03-29 18:48:44 +0500

    Личный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -2,14 +2,22 @@
 
 const express = require(`express`);
 const app = express();
+const path = require(`path`);
+
 const mainRoutes = require(`./routes/main`);
 const myRoutes = require(`./routes/my`);
-const offersRoutes = require(`./routes/offers`);
+const articlesRouter = require(`./routes/articles`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
-app.use(`/offers`, offersRoutes);
+app.use(`/articles`, articlesRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту ${DEFAULT_PORT}`));
